eb98ae988447143b290c575fdb59ad6a612f716c
cz <lchu976@gmail.com>
2020-10-25 22:29:47 -0400

    3 commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/contactList"
+    //"URI": "mongodb://localhost/contactList"
+    "URI": "mongodb+srv://cz:YW9cUcRP5H3sJPoi@cluster0.ccsec.mongodb.net/COMP229ASSIGNMENT02?retryWrites=true&w=majority"
 }
