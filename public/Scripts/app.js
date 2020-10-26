b0fc1990eca959ebc78fa52dafa4c7edcca2861f
cz <lchu976@gmail.com>
2020-10-25 23:40:26 -0400

    5 commit

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -44,7 +44,7 @@ function sendContactObject() {
             var contactOjbject = button.data('detail');
                       
             modal.find('#nameTextField').val(contactOjbject.name);
-            modal.find('#numberTextField').val(contactOjbject.number);
+            modal.find('#numberTextField').val(contactOjbject.pNumber);
             modal.find('#emailTextField').val(contactOjbject.email);
             document.getElementById('modalDeleteBtn').href = "/contact-list/delete/" + contactOjbject._id;
             document.getElementById("modalForm").action = '/contact-list/edit/' + contactOjbject._id;
@@ -61,44 +61,3 @@ function sendContactObject() {
         
     })
 }
-
-
-/*contact submit information */
-
-/* send information for UserProfile object */
-
-var UserProfile = {};
-function SendToProfile() {
-    var firstName = document.getElementById("firstName");
-    var lastName = document.getElementById("lastName");
-    var contactNumber = document.getElementById("contactNumber");
-    var email = document.getElementById("emailAddress");
-    var message = document.getElementById("message");
-
-    UserProfile.firstName = firstName.value;
-    UserProfile.lastName = lastName.value;
-    UserProfile.contactNumber = contactNumber.value;
-    UserProfile.email = email.value;
-    UserProfile.message = message.value;
-
-    window.location.replace("/home");
-
-    console.log("\n User fisrt name: " + UserProfile.firstName +
-        "\n User last name: " + UserProfile.lastName +
-        "\n User contact number: " + UserProfile.contactNumber +
-        "\n User email: " + UserProfile.email +
-        "\n User messege: " + UserProfile.message);
-
-    alert("Thank you for your information");
-
-}
-
-/*listen to the button event */
-
-var subBtn = document.getElementById("subBtn");
-if (subBtn.addEventListener) {
-    subBtn.addEventListener("click", SendToProfile, false);
-} else if (subBtn.attachEvent) {
-    subBtn.attachEvent("onclick", SendToProfile);
-}
-
